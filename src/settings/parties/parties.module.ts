10a468c512c9021dc699da4fdcdb702952bb035f
Joel M Gaitan <joel.gaitan@hazina.go.tz>
2025-01-03 17:03:59 +0300

    create user

--- src/settings/parties/parties.module.ts
+++ src/settings/parties/parties.module.ts
@@ -11,10 +11,11 @@ import { Bill } from '../../payment/bill/entities/bill.entity';
 import { BillItem } from '../../payment/bill-item/entities/bill-item.entity';
 import { User } from '../../auth/user/entities/user.entity';
 import { Fee } from '../fees/entities/fee.entity';
+import { UserContextService } from '../../auth/user/dto/user.context';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Party, CommonSetup, Notice, Bill, BillItem, User, Fee])],
   controllers: [PartiesController],
-  providers: [PartiesService, CommonSetupService, NoticeService],
+  providers: [PartiesService, CommonSetupService, NoticeService, UserContextService],
 })
 export class PartiesModule {}
