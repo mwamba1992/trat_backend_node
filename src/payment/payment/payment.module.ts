19cf12f3242c02002718ed7a9be37559d97d8db0
Joel M Gaitan <joel.gaitan@hazina.go.tz>
2025-01-06 11:44:07 +0300

    adding fixes of payment report

--- src/payment/payment/payment.module.ts
+++ src/payment/payment/payment.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { UserContextService } from '../../auth/user/dto/user.context';
 
 @Module({
   controllers: [PaymentController],
-  providers: [PaymentService],
+  providers: [PaymentService, UserContextService],
 })
 export class PaymentModule {}
